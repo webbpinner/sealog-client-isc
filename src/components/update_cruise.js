550a99088edd977a444dcd07d1ac4ec7b85e0779
Webb Pinner <webbpinner@gmail.com>
2019-02-14 10:56:38 -0500

    fixed issues where uploaded files were not displayed

--- src/components/update_cruise.js
+++ src/components/update_cruise.js
@@ -433,8 +433,8 @@ function validate(formProps) {
 }
 
 function mapStateToProps(state) {
-  let initialValues = state.cruise.cruise
-
+  
+  let initialValues = { ...state.cruise.cruise }
 
   if (initialValues.cruise_additional_meta) {
     if (initialValues.cruise_additional_meta.cruise_name) {
