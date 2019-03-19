a012771c010853b69be870646bcc8c3ab24c32a3
Webb Pinner <webbpinner@gmail.com>
2019-03-19 11:07:31 -0400

    fixing cruise/lowering file download bug.

--- src/components/cruise_menu.js
+++ src/components/cruise_menu.js
@@ -63,18 +63,19 @@ class CruiseMenu extends Component {
     {
       headers: {
         authorization: cookies.get('token')
-      }
+      },
+      responseType: 'stream'
     })
     .then((response) => {
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', filename); //or any other extension
-      document.body.appendChild(link);
-      link.click();
-      link.parentNode.removeChild(link);
+      // const url = window.URL.createObjectURL(new Blob([response.data]));
+      // const link = document.createElement('a');
+      // link.href = url;
+      // link.setAttribute('download', filename); //or any other extension
+      // document.body.appendChild(link);
+      // link.click();
+      // link.parentNode.removeChild(link);
 
-        // FileDownload(response.data, filename);
+      FileDownload(response.data, filename);
     })
     .catch((error)=>{
       console.log("JWT is invalid, logging out");
@@ -86,18 +87,20 @@ class CruiseMenu extends Component {
     {
       headers: {
         authorization: cookies.get('token')
-      }
+      },
+      responseType: 'stream'
+
     })
     .then((response) => {
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', filename); //or any other extension
-      document.body.appendChild(link);
-      link.click();
-      link.parentNode.removeChild(link);
+      // const url = window.URL.createObjectURL(new Blob([response.data]));
+      // const link = document.createElement('a');
+      // link.href = url;
+      // link.setAttribute('download', filename); //or any other extension
+      // document.body.appendChild(link);
+      // link.click();
+      // link.parentNode.removeChild(link);
 
-        // FileDownload(response.data, filename);
+      FileDownload(response.data, filename);
      })
     .catch((error)=>{
       console.log("JWT is invalid, logging out");
